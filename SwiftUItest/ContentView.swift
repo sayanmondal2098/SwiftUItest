cf58dbbe5603fc4d5b537585d2e346cded5767b7
Sayan Mondal <mondalsayan556@gmail.com>
2019-06-06 00:38:06 +0530

    Initial Commit

---
+++ SwiftUItest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUItest
+//
+//  Created by Sayan Mondal on 06/06/19.
+//  Copyright © 2019 Sayan Mondal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
